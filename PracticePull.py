ffb53362d9560682fba436fb569cc30bb040050f
KDakshayani <karimaddeladakshayani@gmail.com>
2026-09-07 20:44:50 +0530

    Update PracticePull.py

--- PracticePull.py
+++ PracticePull.py
@@ -4,3 +4,6 @@ celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9/5) + 32
 
 print(f"{celsius}°C is equal to {fahrenheit}°F")
+
+
+print(f"Rounded Fahrenheit: {round(fahrenheit, 2)}°F")
